98ec2b32fa2f310cc241728a34200a236e596b36
张鹏玄 <pengxuanzhang@anjuke.com>
2015-11-29 14:51:29 +0800

    添加登录模板

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -16,9 +16,8 @@ class Authenticate
 
     /**
      * Create a new filter instance.
-     *
-     * @param  Guard  $auth
-     * @return void
+    /**
+     * @param Guard $auth
      */
     public function __construct(Guard $auth)
     {
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,13 +1,10 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Laravel</title>
-
-
+        <title>缓存管理系统</title>
         {!! HTML::style('packages/flat-ui/css/vendor/bootstrap.min.css') !!}
         {!! HTML::style('packages/flat-ui/css/flat-ui.min.css') !!}
         {!! HTML::script('packages/flat-ui/js/vendor/flat-ui.min.js') !!}
-
         <style>
             html, body {
                 height: 100%;
@@ -34,14 +31,41 @@
             }
 
             .title {
-                font-size: 96px;
+                font-size: 40px;
             }
         </style>
     </head>
     <body>
         <div class="container">
             <div class="content">
-                <div class="title">Laravel 5</div>
+                <div class="title">cache manage system</div>
+                <form method="post" action="<?php echo $_SERVER['REQUEST_URI']; ?>">
+
+                    <div class="login-form">
+
+                        <div class="form-group">
+
+                            <input name="login_name" type="text" class="form-control login-field" value="" placeholder="Username" id="login-name" />
+
+                            <label class="login-field-icon fui-user" for="login-name"></label>
+
+                        </div>
+
+
+
+                        <div class="form-group">
+
+                            <input  name="login_password" type="password" class="form-control login-field" value="" placeholder="Password" id="login-pass" />
+
+                            <label class="login-field-icon fui-lock" for="login-pass"></label>
+
+                        </div>
+
+                        <input class="btn btn-primary btn-lg btn-block" type="submit"  name="dlf_submit" value="Log in" />
+
+                </form>
+
+            </div>
             </div>
         </div>
     </body>
