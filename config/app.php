56843a5d8dc963f70e406bc15b18bbe2427eaa5c
张鹏玄 <pengxuanzhang@anjuke.com>
2015-11-29 12:43:35 +0800

    忽略文件修改;app-key修改（php artisan key:generate）

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'debug' => env('APP_DEBUG', false),
+    'debug' => env('APP_DEBUG', true),
 
     /*
     |--------------------------------------------------------------------------
@@ -78,7 +78,7 @@ return [
     |
     */
 
-    'key' => env('APP_KEY', 'SomeRandomString'),
+    'key' => env('APP_KEY', 'FDcJntobyo5M3Tiwm0RxSpQa0TNJV2m6'),
 
     'cipher' => 'AES-256-CBC',
 
